63ddfd0e838661b2e99b86524662cdaa7c7e4f13
zhang <884831651@qq.com>
2018-05-08 13:21:48 +0800

    commit

--- leetcode/num.cpp
+++ leetcode/num.cpp
@@ -120,7 +120,9 @@ typedef struct lnode{
     int data;
     struct lnode *next;
 }list;
+
 typedef struct node{
+    int length[10];
     list *hash[10];
 }hash;
 /**
@@ -174,13 +176,95 @@ bool containsDuplicate(int* nums, int numsSize) {
     }
     return false;
 }
-
-
-int main() {
-    int num[] = {};  if(containsDuplicate(num,0)){
-        printf("%s","true");
-    } else{
-        printf("%s", "false");
+/**
+ * 只出现一次的数字
+ * 给定一个非空整数数组，除了某个元素只出现一次以外，
+ * 其余每个元素均出现两次。找出那个只出现了一次的元素。
+ * 这是最差的办法了
+ * @param nums
+ * @param numsSize
+ * @return
+ */
+int singleNumber1(int* nums, int numsSize) {
+    for(int i=0;i<numsSize;i++){
+        for (int j = 0; j <numsSize ; j++) {
+            if(i!=j && nums[i]==nums[j]){
+                nums[i]=NULL;
+                nums[j]=NULL;
+            }
+        }
     }
+    for(int i=0;i<numsSize;i++){
+        if(nums[i]!=NULL){
+            return nums[i];
+        }
+    }
+    return 0;
+}
+/**
+ * 一组数，只有一个单其他是双跳出单
+ * 全部遍历一次，放入hash表，找出长度为一的....
+ * 想复杂了。
+ * 先排序，再一次走两步
+ * @param nums
+ * @param numsSize
+ * @return
+ */
+int singleNumber(int* nums, int numsSize) {
+    hash *h=(hash *)malloc(sizeof(hash));
+    for (int i = 0; i < 10; i++) {
+        h->length[i]=0;
+        h->hash[i]=NULL;
+    }
+    for (int j = 0; j < numsSize; ++j) {
+        list *child,*temp,*t;
+        int key=abs(nums[j]%9);
+        child=h->hash[key];
+        if(child == NULL){
+            temp=(list *)malloc(sizeof(list));
+            temp->data=nums[j];
+            temp->next=NULL;
+            //因为child是NULL没有指向任何地方
+            h->hash[key]=temp;
+        } else{
+            temp=(list *)malloc(sizeof(list));
+            temp->data=nums[j];
+            temp->next=NULL;
+            //相同的放到相邻,不存在相同的肯定放在最后了,尾插法不行，
+            //用头插法
+            if(child->data==nums[j] && child->next != NULL){
+                child=child->next;
+            }
+            while (child->next != NULL && child->data != nums[j]){
+                child=child->next;
+            }
+            t=child->next;
+            child->next=temp;
+            temp->next=t;
+        }
+        h->length[key]++;
+    }
+    //找出不同
+    for(int i=0;i<10;i++){
+        if(h->hash[i]!=NULL && h->length[i]%2!=0){
+            if(h->length[i]==1){
+                return h->hash[i]->data;
+            } else{
+                list *last=h->hash[i];
+                while (last->next!=NULL){
+                    last=last->next;
+                }
+                return last->data;
+            }
+
+        }
+    }
+    return NULL;
+}
+int main() {
+    int num[] = {10,186,-49,176,118,167,-61,189,6,-24,7,-93,71,112,187,45,-36,38,82,108,-46,112,51,165,-37,159,1,-53,7,38,90,181,-23,91,-42,172,-95,130,84,149,-47,68,126,-67,175,22,121,131,84,114,60,64,182,-75,-17,-71,69,-92,103,-91,-91,86,126,166,33,-36,-80,-37,118,-80,-18,127,36,-71,-82,-82,144,12,57,149,71,91,-83,-100,-30,45,186,16,-51,-72,-83,107,140,-97,-93,1,12,189,-61,-50,180,98,96,-29,193,167,57,-45,16,6,-76,4,109,-23,22,144,190,-97,193,-51,-99,-79,-47,142,107,175,109,121,190,90,34,32,63,-24,41,-53,41,89,130,-18,-99,103,86,127,-30,102,32,-49,181,-60,114,60,-29,182,-75,168,96,51,33,142,108,69,10,-57,166,48,82,161,-17,-50,102,63,-45,140,180,176,-95,36,-46,168,187,161,-72,-100,-42,165,-76,-67,89,159,64,34,98,4,-60,172,-79,68,48,131,-57
+    };
+    int s=singleNumber(num,1000);
+    printf("%d",s);
     return 0;
 }
